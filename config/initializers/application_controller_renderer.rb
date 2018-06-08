ae56f1ed3467fe3641290eb46ffa9ba25a914ec6
tomokiyao <tomoki9226@gmail.com>
2018-06-08 11:35:04 +0000

    finish 5-8

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '960fd5ef027faffc4136f159ccf42d0734aa54b166776153e82c5c1e9965cf138f397f09d4c3bc54809331df8f061dd8f47af77d2101c9d8cf247a263731c728'
